a07a96ad4cb330fd6a65ae086dca994dfc84f490
HookSniff <hooksniff@users.noreply.github.com>
2026-06-15 20:36:44 +0800

    fix: correct package name from OpenAPIClient to HookSniff

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "OpenAPIClient",
+    name: "HookSniff",
     platforms: [
         .iOS(.v11),
         .macOS(.v10_13),
@@ -13,7 +13,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "OpenAPIClient",
+            name: "HookSniff",
             targets: ["OpenAPIClient"]
         ),
     ],
@@ -25,7 +25,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "OpenAPIClient",
+            name: "HookSniff",
             dependencies: ["AnyCodable", ],
             path: "OpenAPIClient/Classes"
         ),
